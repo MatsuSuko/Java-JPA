eec8753870de8351b7e4389ff3322dee5e7452f7
Souvanny Bounmy <souvannyb21@gmail.com>
2024-03-12 14:33:49 +0100

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.hibernate.engine.spi.EntityEntryFactory;
+
+public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("demo");
+        EntityManager em = emf.createEntityManager();
+        em.close();
+        emf.close();
+
+
+    }
+
+
+}
